b38f89df788d68ccebc268d81834b10687a2f350
rainbowmoonlight.github.io <46019037+rainbowmoonlight@users.noreply.github.com>
2019-01-09 17:32:40 -0800

    fix

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controllers/image')
 const db= knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-convex-67621',
-    user : 'postgres',
-    password : '',
-    database : 'smart-brain'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
